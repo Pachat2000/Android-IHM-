7a6951387e1b4a8939ae68146061abf09886856f
RAYAN <relsegahy@gmail.com>
2026-02-17 15:10:41 +0100

    add first section of the project

--- app/src/main/java/com/example/projectihm/MainActivity.java
+++ app/src/main/java/com/example/projectihm/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.projectihm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        LinearLayout header1 = findViewById(R.id.header_section1);
+        LinearLayout body1 = findViewById(R.id.body_section1);
+        ImageView arrow1 = findViewById(R.id.ImgArrowSectionUp);
+
+        setupAction(header1, body1, arrow1);
     }
+
+    public void setupAction(LinearLayout header, LinearLayout body, ImageView arrow){
+        header.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(body.getVisibility() == View.VISIBLE){
+                    body.setVisibility(View.GONE);
+                    arrow.animate().rotation(0).setDuration(200).start();
+                }else{
+                    body.setVisibility(View.VISIBLE);
+                    arrow.animate().rotation(180).setDuration(200).start();
+                }
+            }
+        });
+    }
+
 }
