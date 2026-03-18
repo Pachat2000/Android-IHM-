e198443e63d93bdc07b579c59675c0d9fe56a448
RAYAN <relsegahy@gmail.com>
2026-03-18 15:28:25 +0100

    add activity menu + result

--- app/src/main/java/com/example/projectihm/MainActivity.java
+++ app/src/main/java/com/example/projectihm/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.projectihm;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
---
+++ app/src/main/java/com/example/projectihm/MainMenu.java
@@ -0,0 +1,46 @@
+package com.example.projectihm;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainMenu extends AppCompatActivity {
+
+    private static final String TAG = "MainMenu";
+    private Button startActivityBtt;
+    private Button resultBtt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_menu);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+
+        startActivityBtt = findViewById(R.id.startActivityBtt);
+        resultBtt = findViewById(R.id.ResultBtt);
+
+        startActivityBtt.setOnClickListener(view -> {
+            startActivity(new Intent(this, MainActivity.class));
+        });
+
+        resultBtt.setOnClickListener(view -> {
+            startActivity(new Intent(this, MainResult.class));
+        });
+
+
+
+    }
+}
